fbb1379e9df86f1293c1e8059fb0205fee23c940
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2022-08-31 19:22:17 -0300

    fix: arrumei o bug no globalStyles

--- kenzie-fretes/src/services/routes.tsx
+++ kenzie-fretes/src/services/routes.tsx
@@ -1,19 +1,21 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
 
 const MainRoutes = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="*" element={<h2>Página não existe</h2>} />
           <Route index element={<h2>HomePage</h2>} />
           <Route path="/login" element={<h2>Login Page</h2>} />
           <Route path="/register" element={<h2>Register Page</h2>} />
           <Route path="/dashboard" element={<h2>Dashboard Page</h2>} />
+          
+          <Route path="*" element={<h2>Página não existe</h2>} />
         </Routes>
       </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default MainRoutes
+export default MainRoutes;
