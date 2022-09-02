d3119dbeabf2ca55efc64048d14f11c3fa7d1612
Raphael <rapha.gloria@gmail.com>
2022-09-02 19:48:17 -0300

    componente posts do motorista

--- kenzie-fretes/src/services/routes.tsx
+++ kenzie-fretes/src/services/routes.tsx
@@ -1,8 +1,7 @@
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import DriverPosts from "../components/DriverPosts"
+import Login from "../pages/Login"
+import Register from "../pages/Register"
 
 const MainRoutes = () => {
   return (
@@ -13,12 +12,13 @@ const MainRoutes = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<h2>Dashboard Page</h2>} />
-          
+          <Route path="/dashboard/driver/posts" element={<DriverPosts />} />
+
           <Route path="*" element={<h2>Página não existe</h2>} />
         </Routes>
       </BrowserRouter>
     </>
-  );
-};
+  )
+}
 
-export default MainRoutes;
+export default MainRoutes
