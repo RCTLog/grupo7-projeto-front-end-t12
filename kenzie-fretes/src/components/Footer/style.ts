d2ac4236349c04eafac255b8980a6e5d17e7ab2e
Lucas Emanuel da Silva <lsengenheirocivil@gmail.com>
2022-09-02 22:05:25 -0300

    feat: criei pagina de suporte e componentizei footer

---
+++ kenzie-fretes/src/components/Footer/style.ts
@@ -0,0 +1,100 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 80%;
+  margin-top: 3rem;
+  min-height: 10vh;
+
+  .logo {
+    img {
+      display: none;
+    }
+  }
+
+  .info {
+    .contact {
+      display: flex;
+      flex-direction: column;
+      gap: 5px;
+      margin-bottom: 10px;
+      p {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+      }
+    }
+
+    .links {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      text-decoration: underline;
+      .pages {
+        display: flex;
+        flex-direction: column;
+        color: white;
+        gap: 5px;
+        margin-bottom: 10px;
+
+        a {
+          color: inherit;
+          display: flex;
+          align-items: center;
+          gap: 10px;
+        }
+      }
+
+      .networks {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+
+        a {
+          color: inherit;
+        }
+      }
+    }
+
+    .mobile {
+      width: 100%;
+      margin-bottom: 15px;
+      p {
+        font-size: 0.95rem;
+        margin-bottom: 5px;
+      }
+
+      .apps {
+        display: flex;
+        gap: 1rem;
+        
+        img {
+          width: 45%;
+          max-width: 200px;
+        }
+      }
+    }
+  }
+
+  .rtclog {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 15px;
+  }
+
+  @media screen and (min-width: 725px) {
+    .info {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      align-items: flex-start;
+    }
+  }
+  @media screen and (min-width: 1200px) {
+    .info {
+      grid-template-columns: 1fr 1fr 1fr;
+      gap: 2rem;
+    }
+  }
+`;
+
+export default Container;
