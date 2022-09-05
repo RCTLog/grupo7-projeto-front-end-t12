f3ef4642575163995cc7edb32056c5ab9caba576
vinMoreiraHenrique <viniciusmoreirahenrique@gmail.com>
2022-09-05 19:31:24 -0300

    Feat/toasts

--- kenzie-fretes/src/context/LoginContext/LoginProvider.tsx
+++ kenzie-fretes/src/context/LoginContext/LoginProvider.tsx
@@ -1,40 +1,40 @@
-import { createContext, useContext, useEffect, useState } from "react"
+import { createContext, useContext, useEffect, useState } from "react";
 import {
   ILoginApi,
   ILoginData,
   ILoginProps,
   ILoginProvider,
-} from "./Login.interfaces"
-import * as yup from "yup"
-import { useForm } from "react-hook-form"
-import { yupResolver } from "@hookform/resolvers/yup"
-import api from "../../services/api"
+} from "./Login.interfaces";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { toast } from "react-toastify";
+import api from "../../services/api";
 
-const LoginContext = createContext<ILoginProvider>({} as ILoginProvider)
+const LoginContext = createContext<ILoginProvider>({} as ILoginProvider);
 
 const LoginProvider = ({ children }: ILoginProps) => {
-  const [emailError, setEmailError] = useState(false)
-  const [passError, setPassError] = useState(false)
+  const [emailError, setEmailError] = useState(false);
+  const [passError, setPassError] = useState(false);
 
-  const [auth, setAuth] = useState(false)
-  const [loading, setLoading] = useState(true)
+  const [auth, setAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const formSchema = yup.object().shape({
     email: yup
       .string()
       .required(() => {
-        setEmailError(true)
-        return "Insira seu e-mail"
+        setEmailError(true);
+        return "Insira seu e-mail";
       })
       .email(() => {
-        setEmailError(true)
-        return "E-mail invalido"
+        setEmailError(true);
       }),
     password: yup.string().required(() => {
-      setPassError(true)
-      return "Insira sua senha"
+      setPassError(true);
+      return "Insira sua senha";
     }),
-  })
+  });
 
   const {
     register,
@@ -42,20 +42,45 @@ const LoginProvider = ({ children }: ILoginProps) => {
     formState: { errors },
   } = useForm<ILoginData>({
     resolver: yupResolver(formSchema),
-  })
+  });
 
   const onSubmit = (data: ILoginData) => {
-    api.post<ILoginApi>("/login/users", data).then((res) => {
-      setAuth(true)
-      window.localStorage.setItem("@RCTL: Token", res.data.accessToken)
-    })
-  }
+    api
+      .post<ILoginApi>("/login/users", data)
+      .then((res) => {
+        setAuth(true);
+        window.localStorage.setItem("@RCTL: Token", res.data.accessToken);
+
+        toast.success("Login realizado com sucesso! Você será redirecionado.", {
+          toastId: 1,
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+          progress: undefined,
+        });
+      })
+      .catch((error) => {
+        toast.error("Login ou senha inválidos.", {
+          toastId: 1,
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+          progress: undefined,
+        });
+      });
+  };
 
   useEffect(() => {
-    const token = localStorage.getItem("@RCTL: Token")
+    const token = localStorage.getItem("@RCTL: Token");
 
-    token && setAuth(true)
-  }, [auth])
+    token && setAuth(true);
+  }, [auth]);
 
   return (
     <LoginContext.Provider
@@ -73,9 +98,9 @@ const LoginProvider = ({ children }: ILoginProps) => {
     >
       {children}
     </LoginContext.Provider>
-  )
-}
+  );
+};
 
-export const useLogin = () => useContext(LoginContext)
+export const useLogin = () => useContext(LoginContext);
 
-export default LoginProvider
+export default LoginProvider;
