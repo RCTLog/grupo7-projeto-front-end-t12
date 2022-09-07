e4dce3ac12ab0166058f6fbdcfe5ee0461920259
Christ1anGall <christiangall92@icloud.com>
2022-09-06 21:42:12 -0300

    fix/buttonhomepage

--- kenzie-fretes/src/components/Header/index.tsx
+++ kenzie-fretes/src/components/Header/index.tsx
@@ -1,25 +1,27 @@
-import HeaderButton from "../../components/HeaderButton"
-import { Container } from "./style"
+import HeaderButton from "../../components/HeaderButton";
+import { Container } from "./style";
 
 import logo from "../../assets/logo.svg";
 import userImg from "../../assets/truck.png";
 
-import { MdMarkEmailUnread } from "react-icons/md"
-import { BsBell } from "react-icons/bs"
+import { MdMarkEmailUnread } from "react-icons/md";
+import { BsBell } from "react-icons/bs";
 
-import { useNavigate } from "react-router-dom"
-import { useLogin } from "../../context/LoginContext/LoginProvider"
+import { useNavigate } from "react-router-dom";
+import { useLogin } from "../../context/LoginContext/LoginProvider";
 
 const Header = () => {
-  let navigate = useNavigate()
-  const { user } = useLogin()
+  let navigate = useNavigate();
+  const { user } = useLogin();
 
   return (
     <Container>
-      <img className="Logo" src={logo} alt="rctlog logo" /> 
+      <img className="Logo" src={logo} alt="rctlog logo" />
       <div className="buttons">
         <HeaderButton
-          onClick={() => navigate("../login", { replace: true })}
+          onClick={() =>
+            navigate("../dashboard/driver/posts", { replace: true })
+          }
           variant="contained"
         >
           Página Inicial
@@ -29,7 +31,7 @@ const Header = () => {
         <img src={userImg} alt="" />
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
