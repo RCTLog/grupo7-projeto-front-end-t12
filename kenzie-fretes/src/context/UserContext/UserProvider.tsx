90a4b117f98181149a1458b982cdcc874de617f3
Christ1anGall <christiangall92@icloud.com>
2022-09-07 19:59:28 -0300

    fix usercontext

--- kenzie-fretes/src/context/UserContext/UserProvider.tsx
+++ kenzie-fretes/src/context/UserContext/UserProvider.tsx
@@ -2,31 +2,30 @@ import { createContext, useEffect, useState } from "react";
 import api from "../../services/api";
 import { IUserContext, IUserProps } from "./User.interfaces";
 
-export const UserContext = createContext<IUserContext>({} as IUserContext)
+export const UserContext = createContext<IUserContext>({} as IUserContext);
 
 const UserProvider = ({ children }: IUserProps) => {
-    const [currentUser, setCurrentUser] = useState({});
-    const userId = localStorage.getItem("@RCTL: UserId");
+  const [currentUser, setCurrentUser] = useState({});
+  const userId = localStorage.getItem("@RCTL: UserId");
 
-    useEffect(() => {
-        api.get(`/users`).then(({ data }) => {
-            setCurrentUser(data.filter((user: any) => {
-                return user.id == userId
-            }))
-        })
+  useEffect(() => {
+    api.get(`/users`).then(({ data }) => {
+      const user = data.filter((user: any) => {
+        return user.id == userId;
+      });
+      setCurrentUser(user[0]);
+    });
+  }, [userId]);
 
-    }, [userId])
-
-    return (
-        <UserContext.Provider
-            value={{
-                currentUser
-            }}
-        >
-            {children}
-
-        </UserContext.Provider>
-    )
-}
+  return (
+    <UserContext.Provider
+      value={{
+        currentUser,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
 
 export default UserProvider;
